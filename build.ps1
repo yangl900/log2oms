8ab998e2f073fcc25b83a64cf22927bfc271c8dd
Andy <andy.gee@microsoft.com>
2018-08-08 19:50:04 -0700

    1709 and 1803 builds

--- build.ps1
+++ build.ps1
@@ -3,4 +3,5 @@ param (
 )
 
 docker build -f .\windows-container\windows-build\windows-go\Dockerfile -t "$repo/golang:windowsservercore-1803" .
-docker build -f .\windows-container\Dockerfile -t "$repo/log2oms:nanoserver-1803" .
+docker build -f .\windows-container\Dockerfile-1709 -t "$repo/log2oms:nanoserver-1709" .
+docker build -f .\windows-container\Dockerfile-1803 -t "$repo/log2oms:nanoserver-1803" .
--- push.ps1
+++ push.ps1
@@ -4,4 +4,5 @@ param (
 )
 
 docker push "$repo/golang:windowsservercore-1803"
+docker push "$repo/log2oms:nanoserver-1709"
 docker push "$repo/log2oms:nanoserver-1803"
